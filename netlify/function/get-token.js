faed8a5b08069aa648c8fa43e1915b18fe52cbe6
hasanvida <hasan@vida.id>
2026-02-26 13:53:35 +0700

    Create get-token.js

---
+++ netlify/function/get-token.js
@@ -0,0 +1,38 @@
+const fetch = require("node-fetch");
+
+exports.handler = async function () {
+  try {
+    const params = new URLSearchParams();
+    params.append("grant_type", "client_credentials");
+    params.append("scope", "roles");
+    params.append("client_id", process.env.VIDA_CLIENT_ID);
+    params.append("client_secret", process.env.VIDA_CLIENT_SECRET);
+
+    const response = await fetch(
+      "https://qa-sso.vida.id/auth/realms/vida/protocol/openid-connect/token",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: params,
+      }
+    );
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        token: data.access_token,
+        signingKey: process.env.VIDA_SIGNING_KEY,
+      }),
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Token generation failed" }),
+    };
+  }
+};
