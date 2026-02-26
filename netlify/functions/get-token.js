b025eb6fa7dce8f546da95e6b22e587364487856
hasanvida <hasan@vida.id>
2026-02-26 14:15:47 +0700

    Update get-token.js

--- netlify/functions/get-token.js
+++ netlify/functions/get-token.js
@@ -1,27 +1,35 @@
-exports.handler = async function (event, context) {
+exports.handler = async function () {
   try {
-    const response = await fetch("https://api.example.com/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        client_id: process.env.VIDA_CLIENT_ID,
-        client_secret: process.env.VIDA_CLIENT_SECRET
-      })
-    });
+    const params = new URLSearchParams();
+    params.append("grant_type", "client_credentials");
+    params.append("scope", "roles");
+    params.append("client_id", process.env.VIDA_CLIENT_ID);
+    params.append("client_secret", process.env.VIDA_CLIENT_SECRET);
+
+    const response = await fetch(
+      "https://qa-sso.vida.id/auth/realms/vida/protocol/openid-connect/token",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: params,
+      }
+    );
 
     const data = await response.json();
 
     return {
-      statusCode: 200,
-      body: JSON.stringify(data)
+      statusCode: response.status,
+      body: JSON.stringify(data),
     };
 
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: error.message })
+      body: JSON.stringify({
+        error: error.message,
+      }),
     };
   }
 };
